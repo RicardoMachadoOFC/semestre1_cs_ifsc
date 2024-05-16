7597fe0d78520efd4d5c460c0b1659edb8db1437
RicardoMachadoOFC <cado.machado@hotmail.com>
2024-05-16 00:01:38 -0300

    correção

--- Pagina-Lista1/src/Exercicio01.java
+++ Pagina-Lista1/src/Exercicio01.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class Exercicio01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
 
-        System.out.print("Digite o primeiro número: ");
+        System.out.print("Digite o primeiro numero: ");
         double numero1 = scanner.nextDouble();
 
-        System.out.print("Digite o segundo número: ");
+        System.out.print("Digite o segundo numero: ");
         double numero2 = scanner.nextDouble();
 
         double soma = numero1 + numero2;
 
-        System.out.println("A soma dos dois números é: " + soma);
+        System.out.println("A soma dos dois numeros é: " + soma);
       
         scanner.close();
     }
