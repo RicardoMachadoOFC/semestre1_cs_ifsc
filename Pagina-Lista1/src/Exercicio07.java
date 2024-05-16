362d15637da3d6b643484570a555670e3b17b16a
RicardoMachadoOFC <cado.machado@hotmail.com>
2024-05-16 01:54:58 -0300

    atualização de exercicios

---
+++ Pagina-Lista1/src/Exercicio07.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exercicio07 {
+
+    //Escreva um algoritmo para ler quatro números, calcular o quadrado de cada um deles, somar tudo e mostrar o resultado
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        
+    }
+}
